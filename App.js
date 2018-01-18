63779efeda3132da100e402d932c685834c90bff
bakary <papiss8509@gmail.com.com>
2018-01-18 17:52:41 +0100

    mes filters

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
+
+class TaskApp extends React.Component{
+    render(){
+        return(
+            <div>
+                <TaskList />
+            </div>
+        )
+    }
+}
 class TaskList extends React.Component{
     constructor(props) {
         super(props);
@@ -27,20 +37,41 @@ class TaskList extends React.Component{
         };
     }
     render(){
+        const incompleteTask = this.state.task.filter(task => task.complete === false) 
+        const completeTask = this.state.task.filter(task => task.complete === true) 
+
         return(
             <div>
                 <h1>All Tasks</h1>
-                {this.state.task.map(task =>{
-                    return(
-                        <article key={task.id}>
-                            <span>#{task.id} - {task.description} -------- </span>
-                            {task.complete ? 'Bravo' : 'féniant'}
-                        </article>
-                    )
-                })}
+                {this.state.task.map(task => <Tasks key={task.id} task={task} /> )}
+
+                <h1>Tasks incomplete</h1>
+                {incompleteTask.map(task => <Tasks key={task.id} task={task} />)}
+
+                <h1>Tasks complete</h1>
+                {completeTask.map(task => <Tasks key={task.id} task={task} />)}
             </div>
         )
     }
 }
 
-ReactDOM.render(<TaskList />, document.getElementById('root'))
+const Tasks = (props) =>(
+    <article>
+        <span>#{props.task.id} - {props.task.description} -------- </span>
+        {props.task.complete ? 'Bravo' : 'féniant'}
+    </article>
+)
+
+/* class Tasks extends React.Component {
+    render() {
+        return(
+            <article>
+                <span>#{this.props.task.id} - {this.props.task.description} -------- </span>
+                {this.props.task.complete ? 'Bravo' : 'féniant'}
+            </article>
+        )
+    }
+} */
+
+
+ReactDOM.render(<TaskApp />, document.getElementById('root'))
