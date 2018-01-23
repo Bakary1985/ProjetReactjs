02d895f9e293f600a362cd6e196b2f46f92c692e
bakary <papiss8509@gmail.com.com>
2018-01-23 03:33:44 +0100

    formulaire

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 
 const ROUTER={
-    home: '/',
-    incompletetask: '/incompletetask',
-    completetask: '/completetask'
+    home: '#/',
+    incompletetask: '#/incompletetask',
+    completetask: '#/completetask'
 }
 
 class TaskApp extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
+            currentRouter: ROUTER.home,
+            newTask: '',
             task: [
                 {
                     id: 1,
@@ -32,8 +34,15 @@ class TaskApp extends React.Component{
                 }
             ]
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.addTask = this.addTask.bind(this);
     }
-
+    componentDidMount() {
+        window.location.hash = ROUTER.home;
+        window.onhashchange = (e) =>{
+            this.setState({currentRouter: window.location.hash});
+        }
+    }
     Alltask(){
         return this.state.task;
     }
@@ -45,7 +54,7 @@ class TaskApp extends React.Component{
     }
 
     renderRender(){
-        switch (window.location.pathname) {
+        switch (this.state.currentRouter) {
             case ROUTER.home: return <TaskList task={this.Alltask()} title="All task" /> ;       
             case ROUTER.incompletetask: return <TaskList task={this.incompletetask()} title="incomplete task"/>;        
             case ROUTER.completetask: return <TaskList task={this.completetask()} title="complete task"/> ;       
@@ -53,20 +62,41 @@ class TaskApp extends React.Component{
                 
         }
     }
+
+    addTask(e){
+        e.preventDefault();
+        
+        const newTask = {
+            id: this.state.task.length + 1,
+            description: this.state.newTask,
+            complete: false
+        }
+
+        this.setState({task :this.state.task.concat(newTask), newTask: ''})
+    }
+    handleChange(e){
+        this.setState({newTask: e.target.value});
+    }
     render(){
         return(
             <div>
                 <ul>
                     <li><a href={ROUTER.home}>All tasks</a></li>
                     <li><a href={ROUTER.completetask}>complete task</a></li>
-                    <li><a href={ROUTER.incompletetask}>incomplete task</a></li>
-                    
+                    <li><a href={ROUTER.incompletetask}>incomplete task</a></li>                  
                 </ul>
+
+                <form onSubmit={this.addTask}>
+                    <input require type="text" onChange={this.handleChange} value={this.state.newTask} placeholder="tapez votre description" />
+                    <input type="submit" value="Add Task" />
+                </form>
+
                 {this.renderRender()}
             </div>
         )
     }
 }
+
 
 
 const TaskList = (props) => (
